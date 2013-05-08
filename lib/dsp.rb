995d22e276edba2f2a6769b1dd332b3ed429132b
ochiai <ochiai@slis.tsukuba.ac.jp>
2013-05-08 16:57:52 +0900

    namespaces追加

--- lib/dsp.rb
+++ lib/dsp.rb
@@ -3,7 +3,10 @@ require "dsp/version"
 require 'dsp/description_set_template'
 require 'dsp/description_template'
 require 'dsp/statement_template'
+require 'dsp/namespaces'
 require 'string'
 
 module DSP
 end
+
+
