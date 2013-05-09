bad5f9728075765d19d9d284a639b03d3ec896aa
ochiai <ochiai@slis.tsukuba.ac.jp>
2013-05-09 12:52:46 +0900

    method on_classの改変

--- lib/dsp/statement_template.rb
+++ lib/dsp/statement_template.rb
@@ -10,29 +10,47 @@ module DSP
     property :min_qualified_cardinality, :predicate => RDF::OWL.minQualifiedCardinality, :type => Integer
     has_many :statement_templates, :predicate => RDF::RDFS.subClassOf, :type => "DSP::StatementTemplate"
 
+
     # StatementTemplateのowl:onClassを取得する
-    # owl:onClassの先がURIを持つリソースである場合と，URIを持たない空ノードである場合の両者に対応
-    # 以下の2パターンの構造に対応
-    #   st -> owl:onClass -> :class
-    #   st -> owl:onClass -> :bnode(collection) -> owl:oneOf -> :class
+    # 参照値の場合 => owl:oneOfの先にあるクラスを取得
+    # リソースを持つ構造化、または空ノードの場合 => DSP::DescriptionTemplateを返す
     def on_class
       klass = get_on_class(self)
       return nil if klass.nil?
+      return klass if klass.class.to_s == "DSP::DescriptionTemplate"
       collection = get_collection(klass)
       return collection.empty? ? [klass] : collection
     end
 
     # 与えられたノードからowl:onClassを辿った先にあるノードを取得する
     def get_on_class(node)
+      # リソースを持つ構造化の場合
       query = RDF::Query.new do
         pattern [node.uri, RDF::OWL.onClass, :klass]
+        pattern [:klass, RDF::DSP.resourceClass, :resource_class]
+      end
+      query.execute(Spira.repository(:default)).each do |solution|
+        return RDF::URI(solution.klass).as(DescriptionTemplate)
+      end
+      # 参照値の場合
+      query = RDF::Query.new do
+        pattern [node.uri, RDF::OWL.onClass, :klass]
+        pattern [:klass, RDF::OWL.oneOf, :one_of]
       end
       query.execute(Spira.repository(:default)).each do |solution|
         return solution.klass
       end
+      # 空白ノードの場合
+      query = RDF::Query.new do
+        pattern [node.uri, RDF::OWL.onClass, :klass]
+      end
+      query.execute(Spira.repository(:default)).each do |solution|
+        return RDF::URI(solution.klass).as(DescriptionTemplate)
+      end
       return nil
     end
 
+    # 参照値の場合
     # 与えられたノードからowl:oneOfを辿った先にあるオブジェクトノードを取得する
     def get_one_of(node)
       query = RDF::Query.new do
@@ -44,6 +62,7 @@ module DSP
       return nil
     end
 
+    # 参照値の場合
     # 与えられたノードからowl:oneOfを辿った先を起点として，コレクション構成ノードの配列を取得する
     def get_collection(klass)
       node = get_one_of(klass)
@@ -58,6 +77,7 @@ module DSP
       return result
     end
 
+    # 参照値の場合
     # 与えられたコレクション構成ノードの値と次のノードを取得する
     def get_collection_element(node)
       query = RDF::Query.new do
