7ff96d270d997508efd93a537a93344f483a21af
落合香織 <haisaioji@hsioji.local>
2013-10-08 02:20:55 +0900

    namespacesメソッドの変更

--- lib/dsp/description_set_template.rb
+++ lib/dsp/description_set_template.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 module DSP
   class DescriptionSetTemplate < Spira::Base
     include Spira::Resource
@@ -9,5 +10,15 @@ module DSP
     property :created, :predicate => RDF::REG.created, :type => String
     property :registered, :predicate => RDF::REG.registered, :type => String
     #define_type RDF::OWL.Ontology
+
+
+    # rdfファイルがxmlの場合のみ有効
+    def namespaces(fname)
+      namespaces = Hash.new
+      Nokogiri::XML(open(fname).read).namespaces.each do |name,uri|
+        namespaces[name.gsub("xmlns:","")] = uri
+      end
+      return namespaces
+    end
   end
 end
