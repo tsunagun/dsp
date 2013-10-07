c83db7f64e12125f860f6109b41da70641d8046c
落合香織 <haisaioji@hsioji.local>
2013-10-08 02:25:35 +0900

    load fileの変更

--- lib/dsp.rb
+++ lib/dsp.rb
@@ -3,7 +3,6 @@ require "dsp/version"
 require 'dsp/description_set_template'
 require 'dsp/description_template'
 require 'dsp/statement_template'
-require 'dsp/namespaces'
 require 'string'
 
 module DSP
